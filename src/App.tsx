3b1c8618f3d70ffdaf60b69861b20d1ba2f1d557
Ben Follington <5009316+bfollington@users.noreply.github.com>
2022-06-29 15:31:28 +1000

    Fix fxhash bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import "./App.css";
+import './App.css'
 
-const isExport = process.env.REACT_APP_FXHASH;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const Entry = require(isExport ? "./FxHashEntry" : "./GalleryEntry").default;
+const Entry = require(process.env.REACT_APP_FXHASH
+  ? './FxHashEntry'
+  : './GalleryEntry').default
 
 function App() {
   return (
     <div className="App">
       <Entry />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
